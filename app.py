f7f4cdaf088b8c3b688d6971b6ad6cda7d63cf88
zora89 <zorawar.purohit@gmail.com>
2022-06-20 05:19:34 +0530

    nav bar added

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ class LoginForm(FlaskForm):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
